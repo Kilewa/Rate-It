90b238c9323dd72ee678bacf0173ba60af6ae83a
kilewa <kilewageorge230@gmail.com>
2020-10-25 01:26:40 +0300

    projects views

--- users/models.py
+++ users/models.py
@@ -14,7 +14,7 @@ class Profile(models.Model):
     bio = HTMLField(max_length=300,default="No bio")
     profile_photo = models.ImageField(
         default='default.jpg', upload_to='profile_pics')
-    website = URLOrRelativeURLField()     
+    website = URLOrRelativeURLField(default='')     
 
     def __str__(self):
         return f'{self.user.username} Profile'
@@ -38,20 +38,11 @@ class Profile(models.Model):
         profile = cls.objects.get(user=userid)
         return profile
 
-    def save(self, *args, **kwargs):
-        super(Profile, self).save(*args, **kwargs)
-
-        img = Image.open(self.image.path)
-
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
-
-
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    try:
-        instance.profile.save()
-    except ObjectDoesNotExist:
-        Profile.objects.create(user=instance)
+    @receiver(post_save, sender=User)
+    def create_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+     
+    @receiver(post_save, sender=User) 
+    def save_profile(sender,instance,**kwargs):
+        instance.profile.save()  
